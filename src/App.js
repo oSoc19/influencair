f41d239543cc76e4738efe7b9bd656a6a145cda3
toon nelissen appsaloon <toon@appsaloon.be>
2019-07-11 13:02:39 +0200

    Basic layout of the app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <div className="main-container">
-        <div className="header-image" />
-        
-        <div className = "GridContainer">
-            <div className = "Col1">
-            </div>
-            <div className = "Col2">
+    <div className="App GridContainer">
+      <div className="Col1">
+        <SideBar />
+      </div>
+      <div className="Col2">
 
-        </div>
-        </div>
       </div>
     </div>);
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h1>Influencair logo</h1>
+            <h2>Brussels . Air-Quality . Health <br /> What about that?</h2>
+        </div>
+    )
+}
+
+export default Sidebar
