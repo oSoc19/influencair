e600ed117dc9f52dd8a13f76e89f20024a6370fa
Jolan Huyvaert <jolan.huyvaert@student.howest.be>
2019-07-11 10:09:38 +0200

    Added grid layout

--- src/App.js
+++ src/App.js
@@ -5,22 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="main-container">
+        <div className="header-image" />
+        
+        <div className = "GridContainer">
+            <div className = "Col1">
+            </div>
+            <div className = "Col2">
+
+        </div>
+        </div>
+      </div>
+    </div>);
 }
 
 export default App;
