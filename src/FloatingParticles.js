91e48e39329bcb7b3d0fc92baee9871388d7fece
toon nelissen appsaloon <toon@appsaloon.be>
2019-07-16 00:10:37 +0200

    step by step grouping of particles and strarting from spread out particles

--- src/FloatingParticles.js
+++ src/FloatingParticles.js
@@ -30,12 +30,11 @@ class FloatingParticles extends Component {
     ];
     this.margin = { left: 60, top: 20, right: 20, bottom: 20 };
   }
-
   componentDidMount() {
     const { margin, compositionTypes, causes } = this;
     const width = this.props.width - margin.left - margin.right;
     const height = this.props.height - margin.top - margin.bottom;
-    const particles = this.generateParticles();
+    const particles = this.generateParticles(width, height);
 
     this.svg = d3
       .select(this.svgElement.current)
@@ -44,51 +43,59 @@ class FloatingParticles extends Component {
       .append("g")
       .attr("transform", "translate(" + [margin.left, margin.top] + ")");
 
-    this.chargeForce = d3.forceManyBody().strength(-2);
-    this.centerXForce = d3.forceX(width / 2).strength(0);
-    this.centerYForce = d3.forceY(height / 2).strength(0);
+    this.chargeForce = d3
+      .forceManyBody()
+      .strength(-2)
     this.forceCollide = d3
       .forceCollide()
       .strength(1)
       .radius(d => d.r)
-      .iterations(2);
+      .iterations(2)
+    this.xForce = d3
+      .forceX(d => d.x)
+      .strength(0);
+    this.yForce = d3
+      .forceY(d => d.y)
+      .strength(0);
+
     this.simulation = d3
       .forceSimulation()
       .force("charge", this.chargeForce)
       .force("collide", this.forceCollide)
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .force("x", this.centerXForce)
-      .force("y", this.centerYForce);
+      .force("x", this.xForce)
+      .force("y", this.yForce);
 
-    var node = this.svg
+    this.node = this.svg
       .selectAll("circle")
       .data(particles)
       .enter()
       .append("circle")
       .attr("r", d => d.r)
-
-      .attr("fill", d => {
-        switch (d.compositionType) {
-          case compositionTypes[0]:
-            return "#52744D";
-          case compositionTypes[1]:
-            return "#AFCE6D";
-          case compositionTypes[2]:
-            return "#F2F0B5";
-          case compositionTypes[3]:
-            return "#D97C51";
-          case compositionTypes[4]:
-            return "#C12D4E";
-          case compositionTypes[5]:
-            return "#2d66c1";
-          case compositionTypes[6]:
-            return "#972dc1";
-          case compositionTypes[7]:
-            return "#c12d83";
-          default:
-            break;
-        }
-      });
+      .attr("fill", this.compositionColor )
+      .style("opacity", 0)
+      
+    this.compositionColor = d => {
+      switch (d.compositionType) {
+        case compositionTypes[0]:
+          return "#52744D";
+        case compositionTypes[1]:
+          return "#AFCE6D";
+        case compositionTypes[2]:
+          return "#F2F0B5";
+        case compositionTypes[3]:
+          return "#D97C51";
+        case compositionTypes[4]:
+          return "#C12D4E";
+        case compositionTypes[5]:
+          return "#2d66c1";
+        case compositionTypes[6]:
+          return "#972dc1";
+        case compositionTypes[7]:
+          return "#c12d83";
+        default:
+          break;
+      }
+    }
 
     this.compositionTypeScale = d3
       .scalePoint()
@@ -110,7 +117,10 @@ class FloatingParticles extends Component {
 
     this.xCompositionTypeForce = d3.forceX(d =>
       this.compositionTypeScale(d.compositionType)
-    );
+    )
+    this.yCompositionScale = d3.scaleLinear()
+      .domain([0, height])
+      .range([height - margin.bottom, margin.top * 4])
 
     this.causesScale = d3
       .scalePoint()
@@ -130,74 +140,122 @@ class FloatingParticles extends Component {
       .attr("text-anchor", "middle")
       .attr("fill", "transparent");
 
-    this.yCausesFOrce = d3.forceY(d => this.causesScale(d.cause));
+    
+    this.yCausesForce = d3.forceY(d => this.causesScale(d.cause));
+
     this.simulation.nodes(particles).on("tick", () => {
-      node.attr("cx", d => d.x).attr("cy", d => d.y);
+      this.node.attr("cx", d => d.x).attr("cy", d => d.y);
     });
   }
 
   componentWillReceiveProps(nextProps) {
     const { story } = nextProps;
-    if (!story) return;
+    const { isBackwardScroll } = this.props
+
+    // this is when we go back to previous story
+    if (story && story.story === 0 && isBackwardScroll) {
+      this.node.transition().duration(100).ease(d3.easeLinear).style("opacity", 0)
+    }
+
+    // only run this part in story 1
+    if (!story || story.story !== 1) return
+
+    // Don't run anything if we still in the same chapter or subchapter
+    if (story.chapter === this.props.story.chapter && story.subChapter === this.props.story.subChapter) return
 
     const width = this.props.width - this.margin.left - this.margin.right;
     const height = this.props.height - this.margin.top - this.margin.bottom;
-    const { isBackwardScroll } = this.props;
 
-    if (story.subChapter === 1) {
+    
+    let xForce, yForce, centerForce
+
+    if (story.chapter === 0) {
+      xForce = d3.forceX(d => d.posX);
+      yForce = d3.forceY(d => d.posY);
+      centerForce = null
+
+      this.compositionTypesLabels
+          .transition()
+          .attr("x", width / 2)
+          .attr("fill", "transparent")
+
+      this.node.transition().duration(200).ease(d3.easeLinear).style("opacity", 1).attr('fill', this.compositionColor)
+    } else if (story.chapter === 1) {
+      xForce = d3.forceX(d => {
+        // step by step we go through the story 
+        if (d.compositionTypeIndex > story.subChapter) {
+          // Make sure all particles are on the right side of the sorting line
+          if (d.posX < this.compositionTypeScale(this.compositionTypes[story.subChapter + 1])) {
+            return this.compositionTypeScale(this.compositionTypes[story.subChapter + 1]) + (Math.random() * (width - this.compositionTypeScale(this.compositionTypes[story.subChapter + 1]))) 
+          } else {
+            return d.posX
+          }
+        } else {
+          return this.compositionTypeScale(d.compositionType)
+        } 
+        
+      })
+      yForce = d3.forceY(d => this.yCompositionScale(d.posY))
+      centerForce = null
+      this.node.attr('fill', d => {
+        if (d.compositionTypeIndex === story.subChapter) {
+          return this.compositionColor(d)
+        } else {
+          return '#bfbfbf'
+        }
+      })
+
       if (!isBackwardScroll) {
-        this.simulation.force("center", null);
-        this.simulation.force("x", this.xCompositionTypeForce);
         this.compositionTypesLabels
           .transition()
           .attr("x", d => this.compositionTypeScale(d))
           .attr("fill", "#777");
       } else {
-        this.centerXForce = d3.forceX(width / 2);
-        this.centerYForce = d3.forceY(height / 2);
-        this.simulation
-          .force("x", this.centerXForce)
-          .force("y", this.centerYForce);
-        this.compositionTypesLabels
+          this.causesLabels
           .transition()
-          .attr("x", width / 2)
+          .attr("y", height - height / 2)
           .attr("fill", "transparent");
       }
-      this.simulation.alpha(1).restart();
-    } else if (story.subChapter === 2) {
+    } else if (story.chapter === 2) {
+      yForce = this.yCausesForce
+      xForce = this.xCompositionTypeForce
+      centerForce = null
       if (!isBackwardScroll) {
-        this.simulation.force("center", null);
-        this.centerYForce = d3.forceY(height / 2);
-        this.simulation.force("y", this.yCausesFOrce);
         this.causesLabels
           .transition()
           .attr("y", d => this.causesScale(d))
           .attr("fill", "#777");
       } else {
-        this.centerYForce = d3.forceY(height / 2);
-        this.simulation.force("y", this.centerYForce);
-        this.causesLabels
-          .transition()
-          .attr("y", height - height / 2)
-          .attr("fill", "transparent");
+        
       }
-      this.simulation.alpha(1).restart();
+
     }
+    this.simulation
+      .force("x", xForce)
+      .force("y", yForce)
+      .force("center", centerForce)
+    this.simulation.alpha(1).restart();
   }
 
   generateRandomInteger(min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   }
 
-  generateParticles() {
+  generateParticles(width, height) {
     const particles = [];
     for (let index = 0; index < 200; index++) {
+      const compositionTypeIndex = Math.floor(Math.random() * this.compositionTypes.length)
       particles[index] = {
+        x: Math.random() * width,
+        y: Math.random() * height,
         compositionType: this.compositionTypes[
-          Math.floor(Math.random() * this.compositionTypes.length)
+          compositionTypeIndex
         ],
+        compositionTypeIndex,
         cause: this.causes[Math.floor(Math.random() * this.causes.length)],
-        r: Math.floor(Math.random() * 3) * 5
+        r: Math.floor(Math.random() * 3) * 5,
+        posX: Math.random() * width,
+        posY: Math.random() * height
       };
     }
     return particles;
