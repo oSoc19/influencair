5e6d1f6e9c03d49c4d8da21fb199a7f342e03a78
Teodora Popordanoska <teodorapopordanoska@yahoo.com>
2019-07-22 16:55:35 +0200

    Add lineseries per sensor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import SideBar from "./Sidebar";
 import FloatingParticles from "./FloatingParticles";
-import Explanations from './Explanations'
+import Explanations from "./Explanations";
+import { PMPlotComponent } from "./PMPlotComponent";
 import Text from "./Text";
 import story from "./story.json";
 
@@ -23,29 +24,35 @@ export default class App extends Component {
       isBackwardScroll: false,
       blockScroll: false,
       clearBox: new Map()
-    }
+    };
 
-    this.blockScroll = this.blockScroll.bind(this)
-    this.clearBoxHandler = this.clearBoxHandler.bind(this)
+    this.blockScroll = this.blockScroll.bind(this);
+    this.clearBoxHandler = this.clearBoxHandler.bind(this);
   }
   componentDidMount() {
-    window.addEventListener("scroll", (e) => {
+    window.addEventListener("scroll", e => {
       if (this.state.blockScroll) {
-        window.scrollTo(0, this.state.scroll)
-        return
+        window.scrollTo(0, this.state.scroll);
+        return;
       }
-      const currentStory = story[Math.floor(window.scrollY / (scrollHeight / story.length))]
-      const isBackwardScroll = window.scrollY < this.state.scroll
-      if (currentStory.story === this.state.currentStory.story && currentStory.chapter === this.state.currentStory.chapter && currentStory.subChapter === this.state.currentStory.subChapter) return
+      const currentStory =
+        story[Math.floor(window.scrollY / (scrollHeight / story.length))];
+      const isBackwardScroll = window.scrollY < this.state.scroll;
+      if (
+        currentStory.story === this.state.currentStory.story &&
+        currentStory.chapter === this.state.currentStory.chapter &&
+        currentStory.subChapter === this.state.currentStory.subChapter
+      )
+        return;
       this.setState({
         currentStory,
         scroll: window.scrollY,
         isBackwardScroll
-      })
-    })
+      });
+    });
     if (window.scrollY === 0) {
       setTimeout(() => {
-        window.scrollTo(0, window.innerHeight)
+        window.scrollTo(0, window.innerHeight);
       }, 500);
     }
     window.addEventListener("resize", () => {
@@ -58,10 +65,10 @@ export default class App extends Component {
   blockScroll(blockScroll) {
     this.setState({
       blockScroll
-    })
+    });
   }
   clearBoxHandler(e, id, box) {
-    const clearBox = this.state.clearBox
+    const clearBox = this.state.clearBox;
     // if (e === 'set') {
     //   clearBox.set(id, box)
     // } else if (e === 'delete' && clearBox.has('id')) {
@@ -71,50 +78,50 @@ export default class App extends Component {
     // }
     this.setState({
       clearBox
-    })
+    });
   }
   render() {
     return (
       <div className="App" style={{ height: scrollHeight }}>
         {window.innerWidth < minSupportedWidth ||
-          window.innerHeight < minSupportedHeight ? (
-            <div> We don\'t support this size</div>
-          ) : (
-            <div
-              className="GridContainer"
-              style={{ gridTemplateColumns: `${sideBarWidth}px auto` }}
-            >
-              <div className="Col1">
-                <SideBar
-                  story={this.state.currentStory}
-                  scroll={this.state.scroll}
-                />
-              </div>
-              <div className="Col2">
-                <Text
-                  story={this.state.currentStory}
-                  width={this.state.storyWidth}
-                  height={this.state.storyHeight}
-                  isBackwardScroll={this.state.isBackwardScroll}
-                  blockScroll={this.blockScroll}
-                  clearBoxHandler={this.clearBoxHandler}
-                />
-
-                <FloatingParticles
-                  story={this.state.currentStory}
-                  width={this.state.storyWidth}
-                  height={this.state.storyHeight}
-                  isBackwardScroll={this.state.isBackwardScroll}
-                  blockScroll={this.blockScroll}
-                  clearBox={[...this.state.clearBox.values()]}
-                />
-                <Explanations story={this.state.currentStory} />
-              </div>
-              )}
+        window.innerHeight < minSupportedHeight ? (
+          <div> We don\'t support this size</div>
+        ) : (
+          <div
+            className="GridContainer"
+            style={{ gridTemplateColumns: `${sideBarWidth}px auto` }}
+          >
+            <div className="Col1">
+              <SideBar
+                story={this.state.currentStory}
+                scroll={this.state.scroll}
+              />
             </div>
-          )
-        }
+            <div className="Col2">
+              <Text
+                story={this.state.currentStory}
+                width={this.state.storyWidth}
+                height={this.state.storyHeight}
+                isBackwardScroll={this.state.isBackwardScroll}
+                blockScroll={this.blockScroll}
+                clearBoxHandler={this.clearBoxHandler}
+              />
+
+              <FloatingParticles
+                story={this.state.currentStory}
+                width={this.state.storyWidth}
+                height={this.state.storyHeight}
+                isBackwardScroll={this.state.isBackwardScroll}
+                blockScroll={this.blockScroll}
+                clearBox={[...this.state.clearBox.values()]}
+              />
+              <Explanations story={this.state.currentStory} />
+              <PMPlotComponent story={this.state.currentStory} />
+            </div>
+            )}
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
--- src/PMPlotComponent.js
+++ src/PMPlotComponent.js
@@ -2,197 +2,222 @@ import React, { Component } from "react";
 import "./App.css";
 import "../node_modules/react-vis/dist/style.css";
 import {
-    XAxis,
-    YAxis,
-    XYPlot,
-    LineSeries,
-    Hint,
-    Highlight,
-    ChartLabel
-  } from "react-vis";
-import {YearlyAverageDataPM25} from "./YearlyAverageData.js";
-import {DailyAverageDataPM25} from "./dailyAverageData.js";
+  XAxis,
+  YAxis,
+  XYPlot,
+  LineSeries,
+  Hint,
+  Highlight,
+  ChartLabel
+} from "react-vis";
+import { YearlyAverageDataPM25 } from "./YearlyAverageData.js";
+import { DailyAverageDataPM25 } from "./dailyAverageData.js";
+import dailyAverages from "./resources/data/dailyAveragesPerSensor.json";
 
-class PMPlotComponent extends Component{
-    constructor() {
-        super();
-        let lineGraphData = [];
-        
-          YearlyAverageDataPM25.yearlyAverages.map((element, index) =>
-          lineGraphData.push({
-            x: element["year"],
-            y: element["value"]
-          })
-        );  
-        this.state = {
-          hoverValue: null,
-          rangeY: {top:25, bottom:0},
-          graphData: {
-            Data: lineGraphData,
-            TickValues: [
-              "2005","2006","2007",
-              "2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018"
-            ],
-            WHOGuideline: 10,
-            XText: "Years",
-            YText: "PM2.5"
-          }
-        };
-    }
+class PMPlotComponent extends Component {
+  constructor() {
+    super();
+    let lineGraphData = [];
 
-    
-
-    componentWillReceiveProps(nextProps) {
-      
-      const differentStory = this.props.story.story !== nextProps.story.story;
-      const differentChapter = this.props.story.chapter !== nextProps.story.chapter;
-      const differentSubChapter = this.props.story.subChapter !== nextProps.story.subChapter;
-      if(differentStory || differentChapter || differentSubChapter)
-      {
-         console.log("receivedProps");
-         this.prepareDataLineGraph(nextProps);
+    YearlyAverageDataPM25.yearlyAverages.map((element, index) =>
+      lineGraphData.push({
+        x: element["year"],
+        y: element["value"]
+      })
+    );
+    this.state = {
+      sensorData: dailyAverages,
+      hoverValue: null,
+      rangeY: { top: 25, bottom: 0 },
+      graphData: {
+        Data: lineGraphData,
+        TickValues: [
+          "2005",
+          "2006",
+          "2007",
+          "2008",
+          "2009",
+          "2010",
+          "2011",
+          "2012",
+          "2013",
+          "2014",
+          "2015",
+          "2016",
+          "2017",
+          "2018"
+        ],
+        WHOGuideline: 10,
+        XText: "Years",
+        YText: "PM2.5"
       }
-    }
+    };
+  }
 
-    shouldComponentUpdate(nextProps)
-    {
-      const differentStory = this.props.story.story !== nextProps.story.story;
-      const differentChapter = this.props.story.chapter !== nextProps.story.chapter;
-      const differentSubChapter = this.props.story.subChapter !== nextProps.story.subChapter;
-      return differentStory || differentChapter || differentSubChapter;
+  componentWillReceiveProps(nextProps) {
+    const { story } = this.props;
+    const differentStory = story.story !== nextProps.story.story;
+    const differentChapter = story.chapter !== nextProps.story.chapter;
+    const differentSubChapter = story.subChapter !== nextProps.story.subChapter;
+    if (differentStory || differentChapter || differentSubChapter) {
+      this.prepareDataLineGraph(nextProps);
     }
-    
-    prepareDataLineGraph(nextProps) {
-      const {story} = nextProps;
-      console.log("PrepareDataLineGraph - story: ", story.story);
-      if(story && story.story === 3  && story.chapter === 0 && story.subChapter === 1)
-      {
-        console.log("Change to yearly");
-        let lineGraphData = [];
-        
-          YearlyAverageDataPM25.yearlyAverages.map((element, index) =>
-          lineGraphData.push({
-            x: element["year"],
-            y: element["value"]
-          })
-        );  
-    
-        this.setState({
-          rangeY: {top:25, bottom:0},
-          graphData: {
-            Data: lineGraphData,
-            TickValues: [
-              "2005","2006","2007",
-              "2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018"
-            ],
-            WHOGuideline: 10,
-            XText: "Years",
-            YText: "PM2.5"
-          }
-        });
-      }
-      else if (story && story.story === 3 && story.chapter === 0 && story.subChapter === 2)
-      {
-        console.log("Change to daily");
-        let lineGraphData = [];
-        
-          DailyAverageDataPM25.dailyAverages.map((element, index) =>
-          lineGraphData.push({
-            x: element["Date"],
-            y: element["Value"]
-          })
-        );  
-    
-        this.setState({
-          rangeY: {top:40, bottom:0},
-          graphData: {
-            Data: lineGraphData,
-            TickValues: [
-              "01-01-2018","01-02-2018","01-03-2018","01-04-2018","01-05-2018","01-06-2018","01-07-2018","01-08-2018","01-09-2018","01-10-2018",
-              "01-11-2018","01-12-2018","01-01-2019","01-02-2019","01-03-2019","01-04-2019","01-05-2019","01-06-2019","01-07-2019"
-            ],
-            WHOGuideline: 25,
-            XText: "Days",
-            YText: "PM2.5"
-          }
-        });
-      }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { story } = this.props;
+    const differentStory = story.story !== nextProps.story.story;
+    const differentChapter = story.chapter !== nextProps.story.chapter;
+    const differentSubChapter = story.subChapter !== nextProps.story.subChapter;
+    return differentStory || differentChapter || differentSubChapter;
+  }
+
+  prepareDataLineGraph(nextProps) {
+    const { story } = nextProps;
+    if (
+      story &&
+      story.story === 3 &&
+      story.chapter === 0 &&
+      story.subChapter === 1
+    ) {
+      let lineGraphData = [];
+
+      YearlyAverageDataPM25.yearlyAverages.map((element, index) =>
+        lineGraphData.push({
+          x: element["year"],
+          y: element["value"]
+        })
+      );
+
+      this.setState({
+        rangeY: { top: 25, bottom: 0 },
+        graphData: {
+          Data: lineGraphData,
+          TickValues: [
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018"
+          ],
+          WHOGuideline: 10,
+          XText: "Years",
+          YText: "PM2.5"
+        }
+      });
+    } else if (story && story.story === 3 && story.chapter === 0) {
+      let lineGraphData = [];
+
+      DailyAverageDataPM25.dailyAverages.map((element, index) =>
+        lineGraphData.push({
+          x: element["Date"],
+          y: element["Value"]
+        })
+      );
+
+      this.setState({
+        rangeY: { top: 40, bottom: 0 },
+        graphData: {
+          Data: lineGraphData,
+          TickValues: [
+            "01-01-2018",
+            "01-02-2018",
+            "01-03-2018",
+            "01-04-2018",
+            "01-05-2018",
+            "01-06-2018",
+            "01-07-2018",
+            "01-08-2018",
+            "01-09-2018",
+            "01-10-2018",
+            "01-11-2018",
+            "01-12-2018",
+            "01-01-2019",
+            "01-02-2019",
+            "01-03-2019",
+            "01-04-2019",
+            "01-05-2019",
+            "01-06-2019",
+            "01-07-2019"
+          ],
+          WHOGuideline: 25,
+          XText: "Days",
+          YText: "PM2.5"
+        }
+      });
     }
+  }
 
-    render(){
-        const { rangeY} = this.state;
-        console.log("Render");
-        console.log(this.state);
-        if(this.props.story && this.props.story.story === 3 && this.props.story.chapter === 0 && (this.props.story.subChapter === 1 || this.props.story.subChapter === 2))
-        {
-
-        return (
-          <div className="plot">
-            <XYPlot
+  render() {
+    const { rangeY } = this.state;
+    let { story } = this.props;
+    if (
+      story &&
+      story.story === 3 &&
+      story.chapter === 0 &&
+      story.subChapter !== 0
+    ) {
+      return (
+        <div className="plot">
+          <XYPlot
             xType="ordinal"
-            animation={'noWobble'}
-            /* xDomain={
-              rangeRect && [
-                rangeRect.left,
-                rangeRect.right
-              ]
-            } */
-            yDomain={
-              rangeY && [
-                rangeY.bottom,
-                rangeY.top
-              ]
-            }
+            animation={"noWobble"}
+            yDomain={rangeY && [rangeY.bottom, rangeY.top]}
             height={250}
             width={1200}
             margin={{ bottom: 80, left: 50, right: 10, top: 20 }}
           >
             <LineSeries
-              animation={'noWobble'}
+              animation={"noWobble"}
               className="series"
               data={this.state.graphData.Data}
-              // onNearestX={(value, { index }) =>
-              //   this.setState({ hoverValue: value, index })
-              // }
-              // onSeriesMouseOut={() => this.setState({ hoverValue: null })}
             />
-            {console.log(this.state.graphData.Data.length)}
-            {console.log(this.state.graphData.Data[this.state.graphData.Data.length - 1])}
-            
             <LineSeries
-              animation={'noWobble'}
+              animation={"noWobble"}
               className="WHO guideline"
               color="red"
-              data={[{x: this.state.graphData.Data[0].x, y: this.state.graphData.WHOGuideline}, {x: this.state.graphData.Data[this.state.graphData.Data.length - 1].x, y:  this.state.graphData.WHOGuideline}]}
+              data={[
+                {
+                  x: this.state.graphData.Data[0].x,
+                  y: this.state.graphData.WHOGuideline
+                },
+                {
+                  x: this.state.graphData.Data[
+                    this.state.graphData.Data.length - 1
+                  ].x,
+                  y: this.state.graphData.WHOGuideline
+                }
+              ]}
             />
+            {story.subChapter === 3 &&
+              Object.entries(this.state.sensorData).map(([key, value]) => {
+                let sensorLine = [];
+                value.map(element => {
+                  sensorLine.push({
+                    x: element.Date,
+                    y: element.Value
+                  });
+                });
+                return (
+                  <LineSeries
+                    key={key}
+                    animation={"noWobble"}
+                    color="gray"
+                    data={sensorLine}
+                    strokeWidth="0.1px"
+                  />
+                );
+              })}
 
-            {/* {this.state.hoverValue && (
-              <Hint
-                value={this.state.hoverValue}
-                style={{
-                  fontSize: 14,
-                  padding: 14,
-                  margin: 10,
-                  text: {
-                    display: "none"
-                  },
-                  value: {
-                    color: "red"
-                  }
-                }}
-              >
-                <div
-                  style={{
-                    background: " #96b9c2",
-                    padding: "10px",
-                    color: "white"
-                  }}
-                >
-                  <div>PM25:</div>
-                  <div>{this.state.hoverValue.y}</div>
-                </div>
-              </Hint>
-            )} */}
             <YAxis
               style={{
                 line: { stroke: "#ADDDE1" },
@@ -239,32 +264,13 @@ class PMPlotComponent extends Component{
                 className: "axis-label"
               }}
             />
-
-            {/* <Highlight
-              onBrushEnd={area => this.setState({ rangeRectPM25: area })}
-              onDrag={area => {
-                this.setState({
-                  rangeRectPM25: {
-                    bottom:
-                      rangeRectPM25.bottom + (area.top - area.bottom),
-                    left: rangeRectPM25.left - (area.right - area.left),
-                    right:
-                      rangeRectPM25.right - (area.right - area.left),
-                    top: rangeRectPM25.top + (area.top - area.bottom)
-                  }
-                });
-              }}
-            /> */}
           </XYPlot>
-          </div>
-        
-        );
-          }
-          else 
-          {
-            return null;
-          }
+        </div>
+      );
+    } else {
+      return null;
     }
+  }
 }
 
-export {PMPlotComponent};
+export { PMPlotComponent };
