a657847b7473336cf6d69f9e8cc4e451f14e9d5f
Jolan Huyvaert <jolan.huyvaert@student.howest.be>
2019-07-23 16:10:11 +0200

    Changed pm plot component to linear, added background colors, fixed animations

--- src/PMPlotComponent.js
+++ src/PMPlotComponent.js
@@ -6,9 +6,9 @@ import {
   YAxis,
   XYPlot,
   LineSeries,
-  Hint,
-  Highlight,
-  ChartLabel
+  ChartLabel,
+  GradientDefs,
+  AreaSeries
 } from "react-vis";
 import { YearlyAverageDataPM25 } from "./YearlyAverageData.js";
 import { DailyAverageDataPM25 } from "./dailyAverageData.js";
@@ -21,7 +21,7 @@ class PMPlotComponent extends Component {
 
     YearlyAverageDataPM25.yearlyAverages.map((element, index) =>
       lineGraphData.push({
-        x: element["year"],
+        x: new Date(element["year"]).getTime(),
         y: element["value"]
       })
     );
@@ -29,24 +29,26 @@ class PMPlotComponent extends Component {
       sensorData: dailyAverages,
       hoverValue: null,
       rangeY: { top: 25, bottom: 0 },
+      XType: "linear",
       graphData: {
         Data: lineGraphData,
         TickValues: [
-          "2005",
-          "2006",
-          "2007",
-          "2008",
-          "2009",
-          "2010",
-          "2011",
-          "2012",
-          "2013",
-          "2014",
-          "2015",
-          "2016",
-          "2017",
-          "2018"
+            new Date("2005").getTime(),
+            new Date("2006").getTime(),
+            new Date("2007").getTime(),
+            new Date("2008").getTime(),
+            new Date("2009").getTime(),
+            new Date("2010").getTime(),
+            new Date("2011").getTime(),
+            new Date("2012").getTime(),
+            new Date("2013").getTime(),
+            new Date("2014").getTime(),
+            new Date("2015").getTime(),
+            new Date("2016").getTime(),
+            new Date("2017").getTime(),
+            new Date("2018").getTime()
         ],
+        TickFormat: "year",
         WHOGuideline: 10,
         XText: "Years",
         YText: "PM2.5"
@@ -84,31 +86,33 @@ class PMPlotComponent extends Component {
 
       YearlyAverageDataPM25.yearlyAverages.map((element, index) =>
         lineGraphData.push({
-          x: element["year"],
+          x: new Date(element["year"]).getTime(),
           y: element["value"]
         })
       );
 
       this.setState({
         rangeY: { top: 25, bottom: 0 },
+        XType: "linear",
         graphData: {
           Data: lineGraphData,
           TickValues: [
-            "2005",
-            "2006",
-            "2007",
-            "2008",
-            "2009",
-            "2010",
-            "2011",
-            "2012",
-            "2013",
-            "2014",
-            "2015",
-            "2016",
-            "2017",
-            "2018"
+            new Date("2005").getTime(),
+            new Date("2006").getTime(),
+            new Date("2007").getTime(),
+            new Date("2008").getTime(),
+            new Date("2009").getTime(),
+            new Date("2010").getTime(),
+            new Date("2011").getTime(),
+            new Date("2012").getTime(),
+            new Date("2013").getTime(),
+            new Date("2014").getTime(),
+            new Date("2015").getTime(),
+            new Date("2016").getTime(),
+            new Date("2017").getTime(),
+            new Date("2018").getTime()
           ],
+          TickFormat: "year",
           WHOGuideline: 10,
           XText: "Years",
           YText: "PM2.5"
@@ -119,36 +123,38 @@ class PMPlotComponent extends Component {
 
       DailyAverageDataPM25.dailyAverages.map((element, index) =>
         lineGraphData.push({
-          x: element["Date"],
+          x: this.toDate(element["Date"]).getTime(),
           y: element["Value"]
         })
       );
 
       this.setState({
         rangeY: { top: 40, bottom: 0 },
+        XType: "linear",
         graphData: {
           Data: lineGraphData,
           TickValues: [
-            "01-01-2018",
-            "01-02-2018",
-            "01-03-2018",
-            "01-04-2018",
-            "01-05-2018",
-            "01-06-2018",
-            "01-07-2018",
-            "01-08-2018",
-            "01-09-2018",
-            "01-10-2018",
-            "01-11-2018",
-            "01-12-2018",
-            "01-01-2019",
-            "01-02-2019",
-            "01-03-2019",
-            "01-04-2019",
-            "01-05-2019",
-            "01-06-2019",
-            "01-07-2019"
+            this.toDate("01-01-2018").getTime(),
+            this.toDate("01-02-2018").getTime(),
+            this.toDate("01-03-2018").getTime(),
+            this.toDate("01-04-2018").getTime(),
+            this.toDate("01-05-2018").getTime(),
+            this.toDate("01-06-2018").getTime(),
+            this.toDate("01-07-2018").getTime(),
+            this.toDate("01-08-2018").getTime(),
+            this.toDate("01-09-2018").getTime(),
+            this.toDate("01-10-2018").getTime(),
+            this.toDate("01-11-2018").getTime(),
+            this.toDate("01-12-2018").getTime(),
+            this.toDate("01-01-2019").getTime(),
+            this.toDate("01-02-2019").getTime(),
+            this.toDate("01-03-2019").getTime(),
+            this.toDate("01-04-2019").getTime(),
+            this.toDate("01-05-2019").getTime(),
+            this.toDate("01-06-2019").getTime(),
+            this.toDate("01-07-2019").getTime()
           ],
+          TickFormat: "day",
           WHOGuideline: 25,
           XText: "Days",
           YText: "PM2.5"
@@ -157,8 +163,29 @@ class PMPlotComponent extends Component {
     }
   }
 
+  toDate(dateStr) {
+    var parts = dateStr.split("-")
+    return new Date(parts[2], parts[1] - 1, parts[0])
+  }
+
+  formatTick(timeStamp, format)
+  {
+      if(format === "year")
+      {
+          return new Date(timeStamp).getFullYear();
+      }
+      else if (format === "day")
+      {
+        var date = new Date(timeStamp);
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var year = date.getFullYear();
+        return day + "-" + month + "-" + year;
+      }
+  }
+
   render() {
-    const { rangeY } = this.state;
+    const { rangeY, XType } = this.state;
     let { story } = this.props;
     if (
       story &&
@@ -169,54 +196,31 @@ class PMPlotComponent extends Component {
       return (
         <div className="plot">
           <XYPlot
-            xType="ordinal"
+            xType={XType}
             animation={"noWobble"}
+            // xDomain={[this.state.graphData.Data[0].x, this.state.graphData.Data[
+            //   this.state.graphData.Data.length - 1
+            // ].x]}
             yDomain={rangeY && [rangeY.bottom, rangeY.top]}
             height={250}
             width={1200}
             margin={{ bottom: 80, left: 50, right: 10, top: 20 }}
           >
-            <LineSeries
-              animation={"noWobble"}
-              className="series"
-              data={this.state.graphData.Data}
-            />
-            <LineSeries
-              animation={"noWobble"}
-              className="WHO guideline"
-              color="red"
-              data={[
-                {
-                  x: this.state.graphData.Data[0].x,
-                  y: this.state.graphData.WHOGuideline
-                },
-                {
-                  x: this.state.graphData.Data[
-                    this.state.graphData.Data.length - 1
-                  ].x,
-                  y: this.state.graphData.WHOGuideline
-                }
-              ]}
-            />
-            {story.subChapter === 3 &&
-              Object.entries(this.state.sensorData).map(([key, value]) => {
-                let sensorLine = [];
-                value.map(element => {
-                  sensorLine.push({
-                    x: element.Date,
-                    y: element.Value
-                  });
-                });
-                return (
-                  <LineSeries
-                    key={key}
-                    animation={"noWobble"}
-                    color="gray"
-                    data={sensorLine}
-                    strokeWidth="0.1px"
-                  />
-                );
-              })}
+            <GradientDefs>
+                 <linearGradient id="BelowGradient" x1="0" x2="0" y1="0" y2="1">
+                  <stop offset="0%" stopColor="green" stopOpacity={0.3} />
+                  <stop offset="100%" stopColor="green" stopOpacity={0.3} />
+                </linearGradient>
+              </GradientDefs>
+
+            <GradientDefs>
+               <linearGradient id="TopGradient" x1="0" x2="0" y1="0" y2="1">
+                <stop offset="0%" stopColor="red" stopOpacity={0.3} />
+                <stop offset="100%" stopColor="red" stopOpacity={0.3} />
+              </linearGradient>
+            </GradientDefs>
+              
+            
 
             <YAxis
               style={{
@@ -227,6 +231,7 @@ class PMPlotComponent extends Component {
             />
             <XAxis
               tickValues={this.state.graphData.TickValues}
+              tickFormat={v => this.formatTick(v, this.state.graphData.TickFormat)}
               tickLabelAngle={-45}
               style={{
                 title: { fontSize: "18px" },
@@ -264,6 +269,89 @@ class PMPlotComponent extends Component {
                 className: "axis-label"
               }}
             />
+
+              <AreaSeries
+                color={'url(#BelowGradient)'}
+                data={[
+                  {
+                    x: this.state.graphData.Data[0].x,
+                    y0: 0,
+                    y: this.state.graphData.WHOGuideline
+                  },
+                  {
+                    x: this.state.graphData.Data[
+                      this.state.graphData.Data.length - 1
+                    ].x,
+                    y0: 0,
+                    y: this.state.graphData.WHOGuideline
+                  }
+
+                ]}
+              />
+              <AreaSeries
+              color={'url(#TopGradient)'}
+              data={[
+                {
+                  x: this.state.graphData.Data[0].x,
+                  y0: this.state.graphData.WHOGuideline,
+                  y: rangeY.top
+                },
+                {
+                  x: this.state.graphData.Data[
+                    this.state.graphData.Data.length - 1
+                  ].x,
+                  y0: this.state.graphData.WHOGuideline,
+                  y: rangeY.top
+                }
+              ]}
+              
+              />
+
+            <LineSeries
+              animation={"noWobble"}
+              className="series"
+              color="blue"
+              data={this.state.graphData.Data}
+            />
+            
+            {story.subChapter === 3 &&
+              Object.entries(this.state.sensorData).map(([key, value]) => {
+                let sensorLine = [];
+                value.map(element => {
+                  sensorLine.push({
+                    x: this.toDate(element.Date).getTime(),
+                    y: element.Value
+                  });
+                });
+                return (
+                  <LineSeries
+                    key={key}
+                    animation={"noWobble"}
+                    color="gray"
+                    data={sensorLine}
+                    strokeWidth="0.1px"
+                  />
+                );
+              })}
+
+              <LineSeries
+              animation={"noWobble"}
+              className="WHO guideline"
+              color="red"
+              data={[
+                {
+                  x: this.state.graphData.Data[0].x,
+                  y: this.state.graphData.WHOGuideline
+                },
+                {
+                  x: this.state.graphData.Data[
+                    this.state.graphData.Data.length - 1
+                  ].x,
+                  y: this.state.graphData.WHOGuideline
+                }
+              ]}
+            />
+              
           </XYPlot>
         </div>
       );
