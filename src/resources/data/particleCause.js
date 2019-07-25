bf5556b25fe9e615f3babc0db13a206206d30e99
toon nelissen appsaloon <toon@appsaloon.be>
2019-07-25 11:26:56 +0200

    Correct cause values

--- src/resources/data/particleCause.js
+++ src/resources/data/particleCause.js
@@ -1,35 +1,35 @@
 const causes = [
     {
         name: 'Energy production and distribution',
-        amount: 4
+        amount: (0.29/23.09) * 100
     },
     {
         name: 'Energy use in industry',
-        amount: 8
+        amount: (1.23/23.09) * 100
     },
     {
-        name: 'Commercial institutional and households',
-        amount: 56
+        name: 'Commercial, institutional and households',
+        amount: (13.615/23.09) * 100
     },
     {
         name: 'Road transport',
-        amount: 11
+        amount: (3.35/23.09) * 100
     },
     {
         name: 'Non-road transport',
-        amount: 2
+        amount: (0.42/23.09) * 100
     },
     {
         name: 'Industrial porcesses and product use',
-        amount: 10
+        amount: (3.22/23.09) * 100
     },
     {
         name: 'Agriculture',
-        amount: 4
+        amount: (0.64/23.09) * 100
     },
     {
         name: 'Waste',
-        amount: 5
+        amount: (0.69/23.09) * 100
     },
 ]
 
